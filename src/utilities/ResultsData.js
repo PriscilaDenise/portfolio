1f00f86d540b02ff820a0628d53e3403fdfd6dc3
Denise Priscila <denisepriscilamuwanguzi@gmail.com>
2025-03-21 21:49:57 +0300

    portfolio

---
+++ src/utilities/ResultsData.js
@@ -0,0 +1,27 @@
+export const RESULTS =[
+    [
+        {"id": 1, "course_code": "CS101", "course_name": "Essential Hardware and Software Concepts", "score": 87, "credit_units": 4, "year": 2023, "semester": "First"},
+        {"id": 2, "course_code": "CS102", "course_name": "Fundamentals of Computing", "score": 79, "credit_units": 4, "year": 2023, "semester": "First"},
+        {"id": 3, "course_code": "CS103", "course_name": "Structured Programming", "score": 83, "credit_units": 4, "year": 2023, "semester": "First"},
+        {"id": 4, "course_code": "MATH101", "course_name": "Discrete Mathematics", "score": 72, "credit_units": 3, "year": 2023, "semester": "First"},
+        {"id": 5, "course_code": "ENG101", "course_name": "Writing and Study Skills", "score": 81, "credit_units": 3, "year": 2023, "semester": "First"},
+        {"id": 6, "course_code": "THEO101", "course_name": "Understanding the Old Testament", "score": 85, "credit_units": 3, "year": 2023, "semester": "First"},
+      
+        {"id": 7, "course_code": "CS201", "course_name": "Data Structures and Algorithms", "score": 71, "credit_units": 4, "year": 2023, "semester": "Second"},
+        {"id": 8, "course_code": "CS202", "course_name": "Computer Organisation and Architecture", "score": 77, "credit_units": 3, "year": 2023, "semester": "Second"},
+        {"id": 9, "course_code": "STAT201", "course_name": "Probability and Statistics", "score": 80, "credit_units": 3, "year": 2023, "semester": "Second"},
+        {"id": 10, "course_code": "CS203", "course_name": "Database Design and Applications", "score": 73, "credit_units": 4, "year": 2023, "semester": "Second"},
+        {"id": 11, "course_code": "CS204", "course_name": "Local Area Computer Networking", "score": 81, "credit_units": 3, "year": 2023, "semester": "Second"},
+        {"id": 12, "course_code": "THEO201", "course_name": "Understanding The New Testament", "score": 82, "credit_units": 3, "year": 2023, "semester": "Second"},
+        {"id": 13, "course_code": "HLTH201", "course_name": "Health and Wholeness", "score": 84, "credit_units": 3, "year": 2023, "semester": "Second"},
+      
+        {"id": 14, "course_code": "CS301", "course_name": "CS Field Attachment I - Workshop Practice", "score": 76, "credit_units": 3, "year": 2024, "semester": "First"},
+        {"id": 15, "course_code": "CS302", "course_name": "Object Oriented Programming", "score": 95, "credit_units": 4, "year": 2024, "semester": "First"},
+        {"id": 16, "course_code": "CS303", "course_name": "Operating Systems", "score": 71, "credit_units": 3, "year": 2024, "semester": "First"},
+        {"id": 17, "course_code": "CS304", "course_name": "Design and Analysis of Algorithms", "score": 71, "credit_units": 3, "year": 2024, "semester": "First"},
+        {"id": 18, "course_code": "MATH301", "course_name": "Calculus", "score": 79, "credit_units": 3, "year": 2024, "semester": "First"},
+        {"id": 19, "course_code": "CS305", "course_name": "Data Science", "score": 78, "credit_units": 4, "year": 2024, "semester": "First"},
+        {"id": 20, "course_code": "THEO301", "course_name": "Understanding Ethics from a Christian Perspective", "score": 82, "credit_units": 3, "year": 2024, "semester": "First"}
+      ]
+      
+];
